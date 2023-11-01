3ce16a4985bab525d7ff23f5eac7ef727a2c0357
KippTheKippster <43861443+KippTheKippster@users.noreply.github.com>
2023-11-01 10:18:54 +0100

    Add textboxes

--- src/App.jsx
+++ src/App.jsx
@@ -1,33 +1,65 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+
 import './App.css'
 
-function App() {
-  const [count, setCount] = useState(0)
+window.addEventListener("DOMContentLoaded", () => {
+  console.log("Dom loaded")
+  
+})
+
+
+function App() 
+{
+  const [laTexTarget, setLaTexString] = useState(null)
+  const [equationTarget, setEquationString] = useState(null)
+
+  const equationInput = document.getElementById("equationInput")
+  const laTexInput = document.getElementById("laTexInput")
+  const insertButton = document.getElementById("insertButton")
+
+  function onLatexChanged(target)
+  {
+    if (equationInput != null)
+      equationInput.value = laTexInput.value
+
+    setLaTexString(target)
+  }
+
+  function onEquationChanged(target)
+  {
+    if (laTexInput != null)
+      laTexInput.value = equationInput.value
+
+    setEquationString(target)
+  }
+
+  function insertButtonPressed()
+  {
+    if (laTexInput == null)
+      return
+
+    laTexInput.value += "\\pi"
+
+  }
+
+  var a = 
+  [
+    ["\\frac{}{}", 5, "/"],
+    ["\\pi",  -1, "π"],
+    ["\\pass", -1,]
+  ]
 
   return (
     <>
       <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+        <h1>LaTex är koolt</h1>
+        <button onClick={insertButtonPressed} id='insertButton'>π</button>
+        <div id='text-box-container'>
+            <textarea onChange={onLatexChanged} id="laTexInput" className='text-box'/>
+            <textarea onChange={onEquationChanged} id="equationInput" className='text-box'/>
+        </div>
+        <p></p>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </>
   )
 }
