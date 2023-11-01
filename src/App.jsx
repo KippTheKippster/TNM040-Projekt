0cc8d41d2731b18993a8e889d32d8996da3cbeec
KippTheKippster <43861443+KippTheKippster@users.noreply.github.com>
2023-11-01 11:18:38 +0100

    Add latex parser

--- src/App.jsx
+++ src/App.jsx
@@ -1,45 +1,35 @@
 import { useState } from 'react'
-
 import './App.css'
+
+import * as React from "react";
+
+import { MathComponent } from "mathjax-react";
 
 window.addEventListener("DOMContentLoaded", () => {
   console.log("Dom loaded")
-  
 })
-
 
 function App() 
 {
-  const [laTexTarget, setLaTexString] = useState(null)
+  console.log("Wa");
   const [equationTarget, setEquationString] = useState(null)
+  const [insertTarget, setInsert] = useState(null)
 
   const equationInput = document.getElementById("equationInput")
-  const laTexInput = document.getElementById("laTexInput")
   const insertButton = document.getElementById("insertButton")
-
-  function onLatexChanged(target)
-  {
-    if (equationInput != null)
-      equationInput.value = laTexInput.value
-
-    setLaTexString(target)
-  }
 
   function onEquationChanged(target)
   {
-    if (laTexInput != null)
-      laTexInput.value = equationInput.value
-
     setEquationString(target)
   }
 
-  function insertButtonPressed()
+  function onInsertButtonPressed(target)
   {
-    if (laTexInput == null)
+    if (equationInput == null)
       return
 
-    laTexInput.value += "\\pi"
-
+    equationInput.value += "\\pi"
+    setInsert(target)
   }
 
   var a = 
@@ -49,16 +39,19 @@ function App()
     ["\\pass", -1,]
   ]
 
+  let equationText = "";
+  if (equationInput != null)
+    equationText = equationInput.value
+
   return (
     <>
       <div>
         <h1>LaTex är koolt</h1>
-        <button onClick={insertButtonPressed} id='insertButton'>π</button>
+        <button onClick={onInsertButtonPressed} id='insertButton'>π</button>
         <div id='text-box-container'>
-            <textarea onChange={onLatexChanged} id="laTexInput" className='text-box'/>
-            <textarea onChange={onEquationChanged} id="equationInput" className='text-box'/>
+        <textarea onChange={onEquationChanged} id="equationInput" className='text-box'/>
         </div>
-        <p></p>
+        <MathComponent tex={String.raw`${equationText}`} />
       </div>
     </>
   )
