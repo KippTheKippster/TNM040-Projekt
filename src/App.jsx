a071cc7e9fb8ec65c7554f1b84ee4719f9b20b2e
Unyx005 <116083223+Unyx005@users.noreply.github.com>
2023-11-08 17:02:19 +0100

    Css

--- src/App.jsx
+++ src/App.jsx
@@ -6,6 +6,23 @@ import Latex from 'react-latex-next'
 import 'katex/dist/katex.min.css'
 import CodeMirror from '@uiw/react-codemirror';
 
+import {EditorView} from "@codemirror/view"
+
+let baseTheme = EditorView.baseTheme({
+  ".cm-o-replacement": {
+    display: "inline-block",
+    width: ".5em",
+    height: ".5em",
+    borderRadius: ".25em",
+    textAlign: "left"
+  },
+  "&light .cm-o-replacement": {
+    backgroundColor: "#04c"
+  },
+  "&dark .cm-o-replacement": {
+    backgroundColor: "#5bf"
+  }
+})
 
 import symbols from './Symbols.jsx';
 import functions from './Functions.jsx';
@@ -67,6 +84,73 @@ function App() // Här körs appen
     });
   }
 
+ 
+
+  const greek = [
+    ["\\alpha", -1],
+    ["\\beta", -1],
+    ["\\gamma", -1],
+    ["\\Gamma", -1],
+    ["\\delta", -1],
+    ["\\Delta", -1],
+    ["\\epsilon", -1],
+    ["\\varepsilon", -1],
+    ["\\zeta", -1],
+    ["\\eta", -1],
+    ["\\theta", -1],
+    ["\\vartheta", -1],
+    ["\\iota", -1],
+    ["\\kappa", -1],
+    ["\\lambda", -1],
+    ["\\Lambda", -1],
+    ["\\mu", -1],
+    ["\\nu", -1],
+    ["\\xi", -1],
+    ["\\omicron", -1],
+    ["\\Pi", -1],
+    ["\\pi", -1],
+    ["\\varpi", -1],
+    ["\\rho", -1],
+    ["\\varrho", -1],
+    ["\\Sigma", -1],
+    ["\\sigma", -1],
+    ["\\varsigma", -1],
+    ["\\tau", -1],
+    ["\\Upsilon", -1],
+    ["\\Phi", -1],
+    ["\\phi", -1],
+    ["\\varphi", -1],
+    ["\\chi", -1],
+    ["\\Psi", -1],
+    ["\\psi", -1],
+    ["\\Omega", -1],
+    ["\\omega", -1]
+  ];
+  
+  const relation = [
+    ["\\leq", -1]
+  ]
+
+  const logic = [
+    ["\\forall", -1]
+  ]
+
+  const geometry = [
+    ["\\angle", -1]
+  ]
+
+  const arrows = [
+    ["\\rightarrow", -1]
+  ]
+
+  const other_symbols = [
+    ["\\partial", -1]
+  ]
+
+  const symbols = [
+    greek, relation, logic, geometry, arrows, other_symbols
+  ]
+
   //download button funktion.. 
   function onDownloadButtonClicked(e) {
     downloadText("SqueezyLatextEquation", equationString)
@@ -103,6 +187,8 @@ function App() // Här körs appen
 
   return (
     <>
+    
+   
       <div>
         <div className='logo'>
           <img src="/src/Squeezy_LaTex_logo2.svg" alt="Logo" />
@@ -136,7 +222,7 @@ function App() // Här körs appen
           </div>
         </div>
         <div id='text-box-container'>
-          {<CodeMirror onChange={onEquationChanged} readOnly={false} id="equation-input" className='text-box'/>}
+          {<CodeMirror theme={baseTheme} onChange={onEquationChanged} readOnly={false} id="equation-input" className='text-box'/>}
         
         </div>
         <div id='latex-container'>
@@ -147,7 +233,7 @@ function App() // Här körs appen
         <button type='button' onClick={onMathmlClicked}> </button>
 
         </div>
-        </div>
+      </div>
 
     </>
   );
