adbca7a2aadd647245582ed04da4f619ad36c86f
KippTheKippster <43861443+KippTheKippster@users.noreply.github.com>
2023-11-22 15:09:17 +0100

    Begin Latex caret

--- src/App.jsx
+++ src/App.jsx
@@ -26,21 +26,62 @@ function App() // Här körs appen
   console.log("App called");
 
   const [equationString, setEquationString] = useState("")
+  const [elementIndex, setElementIndex] = useState(0)
+
+
+  function onKeyDown(event)
+  {
+    if (event.code == "ArrowRight")
+    {
+      setElementIndex(elementIndex + 1);
+    }
+    else if (event.code == "ArrowLeft")
+    {
+      setElementIndex(elementIndex - 1);
+    }
+  }
+
+
+  //document.removeEventListener('keydown', onKeydown); //Javascript big pee pee poo poo
+  //document.addEventListener('keydown', onKeydown, true);
 
   //runs after render
   useEffect(() => 
   {
-    let semantics = document.querySelector("#latex-container span");
-    console.log(semantics); 
-    if (semantics == null) //No renderable equation.
-      return;
-
-    console.log("adding")
-    semantics.addEventListener("click", function(e){
-      //alert("You clicked " + e + " times");
-      console.log(e)
-    });
+    const elements = MathMLReader.getAllSpanElements()
+    let index = Math.min(Math.max(elementIndex, 0), elements.length)
+    console.log(elements)
+    console.log("Final: " + index)
+    setLaTeXCaret(elements[index])    
+    document.addEventListener('keydown', onKeyDown);
+    return () => {
+      document.removeEventListener('keydown', onKeyDown);
+    };
   });
+
+  function setLaTeXCaret(element)
+  {
+    const caret = document.getElementById("latex-caret");
+
+    if (element == null)
+    {
+      caret.style.minWidth = 0 + "px";
+      caret.style.minHeight = 0 + "px";  
+      return;
+    }
+
+    let rect = element.getBoundingClientRect();
+
+    let h = (rect.bottom - rect.top)
+    let w = Math.max(h / 20, 1);
+    let x = window.scrollX + rect.right;
+    let y = window.scrollY + rect.top;
+
+    caret.style.minWidth = w + "px";
+    caret.style.minHeight = h + "px";
+    caret.style.left = x + "px";
+    caret.style.top = y + "px"
+  }
 
   function onEquationChanged(e)
   {
@@ -66,7 +107,7 @@ function App() // Här körs appen
 
   function onMathmlClicked()
   {
-    const elements = MathMLReader.getMathMLElementsByCode("\\pi");
+    const elements = MathMLReader.getSpanElementsByCode("\\pi");
     console.log("Got elenemts: ");
     console.log(elements);
   }
@@ -92,6 +133,7 @@ function App() // Här körs appen
         </div>
         <div>
           {/* Creates a button for every element in the first row of symbols array */}
+          <button type='button'onClick={onMathmlClicked}>Do The Thing</button>
           {symbols.map((symbol, index) => (
             <button key={index} onClick={() => onInsertButtonPressed(symbol[0])}>
               {<Latex>{String.raw`$${symbol[0]}$`}</Latex>}
@@ -103,10 +145,10 @@ function App() // Här körs appen
         </div>
         <div id='latex-container'>
           <Latex>{String.raw`$${equationString}$`}</Latex>
+          <div id='latex-caret'></div>
         </div>
         <div className='Buttons'> 
         <button type="button" onClick={onDownloadButtonClicked}>Download</button>
-        <button type='button' onClick={onMathmlClicked}> </button>
 
         </div>
         </div>
--- src/mathml/MathMLReader.jsx
+++ src/mathml/MathMLReader.jsx
@@ -1,20 +1,47 @@
 import { MathMLToLaTeX } from 'mathml-to-latex';
+import { symbol } from 'prop-types';
 
 const semanticsSelector = "#latex-container span span span.katex-mathml math semantics";
+let mathMLList = []
 
-function getChildrenWithSymbol(root, code, list) {
+function mathMLToLatexInterpreter(child, code)
+{
+    if (child.children.length != 0)
+        return null;
+
+    const outerHTML = "<math>" + child.outerHTML + "</math>";        
+    if (MathMLToLaTeX.convert(outerHTML) == code)
+        return child;
+
+    return null
+}
+
+function mathMLToSpanInterpreter(child)
+{
+    if (mathMLList.length == 0 || child.children.length != 0)
+        return null;
+
+    if (child.textContent == mathMLList[0].textContent)
+    {
+        mathMLList.shift();
+        return child;
+    }
+    return null;
+}
+
+function getChildrenWithSymbol(root, list, interpreter, ...args)
+{
     for (let i = 0; i < root.children.length; i++) 
     {
         const child = root.children[i]
-        if (child.children.length == 0) //Is a text element
+        //if (child.children.length == 0) //Is a text element
         {
-            //const outerHTML = "<math>" + "<" + child.nodeName + ">"  + child.innerHTML + "</" + child.nodeName + ">" + "</math>";
-            const outerHTML = "<math>" + child.outerHTML + "</math>";
-            if (MathMLToLaTeX.convert(outerHTML) == code)
-                list.push(child);
+            const result = interpreter(child, args)
+            if (result != null)
+                list.push(result)
         }
 
-        getChildrenWithSymbol(child, code, list);
+        getChildrenWithSymbol(child, list, interpreter, args);
     }
 
     return list;
@@ -22,11 +49,42 @@ function getChildrenWithSymbol(root, code, list) {
 
 function getMathMLElementsByCode(code) 
 {
-    let semantics = document.querySelector("#latex-container span span span.katex-mathml math semantics");
+    const semantics = document.querySelector("#latex-container span span span.katex-mathml semantics");
     if (semantics == null) //No eqution being shown.
         return [];
 
-    return getChildrenWithSymbol(semantics, code, []);
+    return getChildrenWithSymbol(semantics, [], mathMLToLatexInterpreter, code);
 }
 
-export {getMathMLElementsByCode, semanticsSelector};
+function getAllSpanElements() 
+{
+    const base = document.querySelector("#latex-container span span span.katex-html span");
+    if (base == null) //No eqution being shown.
+        return [];
+
+    const list = getChildrenWithSymbol(base, [], (child) => 
+    {
+        const text = child.textContent.replace(/\u200B/g,'') // Removes big funny whitespace chars
+        if (child.children.length == 0 && text != "")
+        {
+            console.log("code: " + child.textContent.charCodeAt(0) + ": " + child.textContent)
+            return child;
+        }
+
+        return null
+    });
+    return list;
+}
+
+function getSpanElementsByCode(code) 
+{
+    const base = document.querySelector("#latex-container span span span.katex-html span");
+    if (base == null) //No eqution being shown.
+        return [];
+
+    mathMLList = getMathMLElementsByCode(code); //Slow and stupid
+    const list = getChildrenWithSymbol(base, [], mathMLToSpanInterpreter)
+    return list;
+}
+
+export {getMathMLElementsByCode, getSpanElementsByCode, getAllSpanElements, semanticsSelector};
